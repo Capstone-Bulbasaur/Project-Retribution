40d19abebed07a040a9e76b0bf592e28c848b547
Mark <mark.filliph@gmail.com>
2022-02-10 20:57:06 -0500

    stubbing out Pot and Image class

---
+++ Assets/Scripts/Image.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Image : MonoBehaviour
+{
+    public int ID;
+    public string Path ;
+}
