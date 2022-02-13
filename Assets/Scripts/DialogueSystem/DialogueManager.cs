880a62eb642387c55fc0e4841acf11db3ce98afa
Valeria Rincon <v.rincon558@gmail.com>
2022-02-13 15:30:59 -0500

    Finished dialogue system and added close button to conversation

--- Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -2,22 +2,29 @@
 
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public TextMeshProUGUI speakerName, dialogue, navButtonText;
+    public Button button;
+    public Sprite nextDialogueSprite;
+    public Sprite closeDialogueSprite;
     public Image speakerSprite;
 
     private int currentIndex;
     private Conversation currentConvo;
     private static DialogueManager instance;
+    private Animator animator;
+    private Coroutine typing;
 
     private void Awake()
     {
         if(instance == null)
         {
             instance = this;
+            animator = GetComponent<Animator>();
         }
         else
         {
@@ -27,6 +34,7 @@ public class DialogueManager : MonoBehaviour
 
     public static void StartConversation(Conversation convo)
     {
+        instance.animator.SetBool("isOpen", true);
         instance.currentIndex = 0;
         instance.currentConvo = convo;
         instance.speakerName.text = "";
@@ -38,9 +46,53 @@ public class DialogueManager : MonoBehaviour
 
     public void ReadNext()
     {
+        if(currentIndex > currentConvo.GetLength())
+        {
+            instance.animator.SetBool("isOpen", false);
+            button.GetComponent<Image>().sprite = nextDialogueSprite;
+            return;
+        }
+
         speakerName.text = currentConvo.GetLineByIndex(currentIndex).speaker.GetName();
-        dialogue.text = currentConvo.GetLineByIndex(currentIndex).dialogue;
+
+        if(typing == null)
+        {
+            typing = instance.StartCoroutine(TypeText(currentConvo.GetLineByIndex(currentIndex).dialogue));
+        }
+        else
+        {
+            instance.StopCoroutine(typing);
+            typing = null;
+            typing = instance.StartCoroutine(TypeText(currentConvo.GetLineByIndex(currentIndex).dialogue));
+        }
+
         speakerSprite.sprite = currentConvo.GetLineByIndex(currentIndex).speaker.GetSprite();
         currentIndex++;
+
+        if(currentIndex >= currentConvo.GetLength() + 1)
+        {
+            button.GetComponent<Image>().sprite = closeDialogueSprite;
+        }
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        dialogue.text = "";
+        bool complete = false;
+        int index = 0;
+
+        while (!complete)
+        {
+            dialogue.text += text[index];
+            index++;
+            yield return new WaitForSeconds(0.03f);
+
+            if(index == text.Length)
+            {
+                complete = true;
+            }
+        }
+        
+        typing = null;
     }
 }
