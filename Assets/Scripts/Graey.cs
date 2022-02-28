681dcf1e8bdac275a414632adf84d95567d28df4
Valeria Rincon <v.rincon558@gmail.com>
2022-02-28 00:29:48 -0500

    Added health bar to Graey
    Health bar script will be used for Isarr as well

--- Assets/Scripts/Graey.cs
+++ Assets/Scripts/Graey.cs
@@ -4,16 +4,43 @@ using UnityEngine;
 
 public class Graey : MonoBehaviour
 {
-    public float health;
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    
     SpriteRenderer spriteRenderer;
     Rigidbody rigidbody;
 
     private PowerSwitcher powerSwitcher;
 
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        // Health bar tester
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    // Health bar tester
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
     public void Attack()
     {
         powerSwitcher = GetComponent<PowerSwitcher>();
     }
+
+
 
     private void PickupWaterPower()
     {
