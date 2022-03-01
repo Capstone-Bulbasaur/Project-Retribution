06b0d73b5f2471433742ba87c2c1c96c92e46ef8
Seanlegroulx <55590972+Seanlegroulx@users.noreply.github.com>
2022-03-01 16:25:52 -0500

    added NPC contoller and minion sprite

---
+++ Assets/IsarrTargetLocation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsarrTargetLocation : MonoBehaviour
+{
+    public Transform PlayerPos;
+    public Transform IsarrPos;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = IsarrPos.position - PlayerPos.position;
+        //transform.position =  PlayerPos.position - IsarrPos.position;
+    }
+}
