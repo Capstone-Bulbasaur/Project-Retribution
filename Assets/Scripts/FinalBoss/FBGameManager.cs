9817183e523cc13d8d2f1578512b40f385907fa3
SteveDave <72089446+Steven-David215@users.noreply.github.com>
2022-03-02 13:26:16 -0500

    created and stubbed out FBManager

---
+++ Assets/Scripts/FinalBoss/FBGameManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FBGameManager : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject isarr;
+    public GameObject minion;
+    public GameObject powerPrefabs;
+    public GameObject spawnPoints;
+    public int maxMinionsOnScreen;
+    public int totalMinions;
+    public int minSpawnTime; //UML has this as a float, anyone care?
+    public int maxSpawnTime; //UML also says float
+
+    private void SpawnMinions()
+    {
+
+    }
+
+    private void RemoveEnemy()
+    {
+
+    }
+
+    private void PauseMenu()
+    {
+
+    }
+
+    private void GameOver()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
