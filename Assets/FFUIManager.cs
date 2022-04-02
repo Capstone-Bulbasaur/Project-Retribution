ef103718cf314d5df5a01a9d24d8659b7e7418b1
SteveDave <72089446+Steven-David215@users.noreply.github.com>
2022-04-02 10:58:45 -0400

    Copied base timer logic from RR
    need to link to the fire script, possibly play with spawn rates after set time passes, also have spawns wait 6 seconds at start for instructions to pass.

--- Assets/FFUIManager.cs
+++ Assets/FFUIManager.cs
@@ -5,20 +5,48 @@ using UnityEngine.UI;
 using GameObject = UnityEngine.GameObject;
 using Random = UnityEngine.Random;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class FFUIManager : MonoBehaviour
 {
     //I won't lie team, beyond adding a timer, I don't know what this Manager is for.
+    //ha, called it. To credit accordingly: Based on timer logic used in RestarauntRush
+    // Primary game time set up
+    public float CurrentTime;
+    public float StartingTime = 100;
+    public TextMeshProUGUI Timer;
 
-    // Start is called before the first frame update
+    private bool isGameOver = false;
+    private float WaitforInstructions = 6.0f; // TODO - fix this hardcoded after Level Up.
+
+    void Awake()
+    {
+        CurrentTime = StartingTime;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isGameOver)
+        {
+            WaitforInstructions -= Time.deltaTime;
+            if (WaitforInstructions < 0)
+            {
+                if (CurrentTime <= 0)
+                {
+                    isGameOver = true;
+                    return;
+                }
+                else
+                {
+                    CurrentTime -= 1 * Time.deltaTime;
+                }
+            }
+            Timer.text = CurrentTime.ToString("0");
+        }
     }
 }
